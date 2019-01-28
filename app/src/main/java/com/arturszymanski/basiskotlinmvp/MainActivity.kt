dcd4ee0ee3b336492d1afc3ab12e88f16b82c23e
arturszymanski <artur.szymanski@smagabakery.com>
2019-01-28 12:47:29 +0100

    [BKMVP-2] change list:
    - Added base presenter implementation
    
    - Added views ( fragment, activity, dialog fragment ) that implements basis presenter functionalities

--- app/src/main/java/com/arturszymanski/basiskotlinmvp/MainActivity.kt
+++ app/src/main/java/com/arturszymanski/basiskotlinmvp/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.arturszymanski.basiskotlinmvp
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.ViewModel
 import android.os.Bundle
+import android.util.Log
+import com.arturszymanski.basiskotlinmvp.view.base.BaseActivity
+import com.arturszymanski.curriculumvitae.presenter.base.PresenterFactory
+import com.arturszymanski.presenter.TestView
+import com.arturszymanski.presenter.TestPresenter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<TestPresenter, TestView>(), TestView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        presenter.doSomething()
+    }
+
+    override fun onPresenterPrepared(fromStorage: Boolean) {
+    }
+
+    override fun presenterClass(): Class<TestPresenter> {
+        return TestPresenter::class.java
+    }
+
+    override fun prepareFactory(): PresenterFactory {
+        return object : PresenterFactory() {
+            override fun <T : ViewModel> createPresenter(presenterClass: Class<T>): T {
+                return TestPresenter() as T
+            }
+        }
+    }
+
+    override fun doSomething() {
+        Log.d("TAG", "View doing something")
     }
 }
---
+++ app/src/main/java/com/arturszymanski/basiskotlinmvp/view/base/BasePresenterDialogFragment.kt
@@ -0,0 +1,68 @@
+package com.arturszymanski.basiskotlinmvp.view.base
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import com.arturszymanski.curriculumvitae.presenter.base.PresenterFactory
+import com.arturszymanski.presenter.base.BaseView
+import com.arturszymanski.presenter.base.BasePresenter
+
+/**
+ * Base Dialog Fragment that adds support for presenter and allow to keep state of this presenter. Also handles basis
+ * presenter methods and needs.
+ */
+abstract class BasePresenterDialogFragment <P, V> : DialogFragment() where V : BaseView, P : BasePresenter<V> {
+
+    /**
+     * Instance of presenter.
+     */
+    lateinit var presenter : P
+    /**
+     * Instance of presenter factory that allow to create presenter and check if was previously created.
+     */
+    private lateinit var presenterFactory : PresenterFactory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        presenterFactory = prepareFactory()
+
+        presenter = ViewModelProviders.of(this, presenterFactory).get(presenterClass())
+        onPresenterPrepared(fromStorage = !presenterFactory.isNewCreated)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(::presenter.isInitialized) {
+            presenter.bind(this as V)
+        }
+    }
+
+    override fun onStop() {
+        if(::presenter.isInitialized) {
+            presenter.unbind()
+        }
+
+        super.onStop()
+    }
+
+    /**
+     * Invoked when presenter was prepared and ready for use in this view. Might be used to setup initial presenter
+     * parameters, eg. from Bundle
+     * @param fromStorage inform if presenter was created (fromStorage = false) or restored (fromStorage = true)
+     */
+    abstract fun onPresenterPrepared(fromStorage: Boolean)
+
+    /**
+     * Method needed to create instance of presenter.
+     * @return Return class of presenter used by this view. eg. BasePresenter::class.java
+     */
+    abstract fun presenterClass(): Class<P>
+
+    /**
+     * Method that create presenter factory that will be used to create presenter.
+     * @return PresenterFactory that will be used to create presenter.
+     */
+    abstract fun prepareFactory() : PresenterFactory
+}
